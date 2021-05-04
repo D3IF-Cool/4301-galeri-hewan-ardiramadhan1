0a0a913de303ada05aad49d70594dfac2fc02f55
ardi <ardi.ramdhan32@gmail.com>
2021-05-04 21:00:43 +0700

    Menambahkan loading indicator

--- app/src/main/java/org/d3if0066/galerihewan/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3if0066/galerihewan/ui/main/MainFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import org.d3if0066.galerihewan.model.Hewan
 import org.d3if0066.galerihewan.R
 import org.d3if0066.galerihewan.databinding.FragmentMainBinding
+import org.d3if0066.galerihewan.network.ApiStatus
 
 class MainFragment : Fragment() {
     private val viewModel: MainViewModel by lazy {
@@ -44,6 +45,24 @@ class MainFragment : Fragment() {
         viewModel.getData().observe(viewLifecycleOwner, {
             myAdapter.updateData(it)
         })
+        viewModel.getStatus().observe(viewLifecycleOwner, {
+            updateProgress(it)
+        })
+
+    }
+    private fun updateProgress(status: ApiStatus) {
+        when (status) {
+            ApiStatus.LOADING -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            ApiStatus.SUCCESS -> {
+                binding.progressBar.visibility = View.GONE
+            }
+            ApiStatus.FAILED -> {
+                binding.progressBar.visibility = View.GONE
+                binding.networkError.visibility = View.VISIBLE
+            }
+        }
     }
 
 }
