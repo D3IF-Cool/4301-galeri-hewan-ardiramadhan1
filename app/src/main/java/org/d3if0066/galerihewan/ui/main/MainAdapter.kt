852941d53ac25bb75e5898f85ed8f65a2ee9e117
ardi <ardi.ramdhan32@gmail.com>
2021-05-04 20:56:57 +0700

    Menampilkan gambar dari internet

--- app/src/main/java/org/d3if0066/galerihewan/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if0066/galerihewan/ui/main/MainAdapter.kt
@@ -3,8 +3,11 @@ package org.d3if0066.galerihewan.ui.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import org.d3if0066.galerihewan.R
 import org.d3if0066.galerihewan.model.Hewan
 import org.d3if0066.galerihewan.databinding.ListItemBinding
+import org.d3if0066.galerihewan.network.HewanApi
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     private val data = mutableListOf<Hewan>()
@@ -15,12 +18,17 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     }
 
     class ViewHolder(private val binding: ListItemBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-                fun bind(hewan: Hewan) = with(binding) {
-                    namaTextView.text = hewan.nama
-                    latinTextView.text = hewan.namaLatin
-                    imageView.setImageResource(hewan.imageResId)
-                }
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(hewan: Hewan) {
+            with(binding) {
+                namaTextView.text = hewan.nama
+                latinTextView.text = hewan.namaLatin
+                Glide.with(imageView.context)
+                    .load(HewanApi.getHewanUrl(hewan.imageId))
+                    .error(R.drawable.ic_baseline_broken_image_24)
+                    .into(imageView)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
