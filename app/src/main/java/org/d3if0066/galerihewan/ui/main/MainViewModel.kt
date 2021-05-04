e57bcc002faaf535d146b2b178f54a2665c9c73b
ardi <ardi.ramdhan32@gmail.com>
2021-05-04 20:47:47 +0700

    Mengambil data dari internet

---
+++ app/src/main/java/org/d3if0066/galerihewan/ui/main/MainViewModel.kt
@@ -0,0 +1,47 @@
+package org.d3if0066.galerihewan.ui.main
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.d3if0066.galerihewan.R
+import org.d3if0066.galerihewan.model.Hewan
+import org.d3if0066.galerihewan.network.HewanApi
+
+class MainViewModel : ViewModel() {
+    private val data = MutableLiveData<List<Hewan>>()
+    init {
+        data.value = initData()
+        retrieveData()
+    }
+
+    private fun retrieveData() {
+        viewModelScope.launch {
+            try {
+                val result = HewanApi.service.getHewan()
+                Log.d("MainViewModel", "Success: $result")
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Failure: ${e.message}")
+            }
+        }
+    }
+
+    // Data ini akan kita ambil dari server di langkah selanjutnya
+    private fun initData(): List<Hewan> {
+        return listOf(
+            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
+            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
+            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
+            Hewan("Domba", "Ovis ammon", R.drawable.domba),
+            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
+            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
+            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
+            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
+            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
+            Hewan("Sapi", "Bos taurus", R.drawable.sapi),
+        )
+    }
+    fun getData(): LiveData<List<Hewan>> = data
+}
